265e63f6a97e07c93cd1063ab51395e6e0486060
Salil Desai <salilsdesai@fb.com>
2023-09-06 17:14:44 -0700

    [Decomposition] full_like (#108537)
    Summary:
    
    Moving decomposition from _inductor/decomposition.py (plus modifying it to use memory_format) and include it in core_aten_decompositions
    
    Test Plan: Phabricator + OSS Tests
    
    Differential Revision: D48939842

--- torch/_inductor/decomposition.py
+++ torch/_inductor/decomposition.py
@@ -344,28 +344,6 @@ def randn_like(self, *, dtype=None, device=None, **kwargs):
     )
 
 
-@register_decomposition(aten.full_like)
-def full_like(
-    self,
-    fill_value,
-    *,
-    dtype=None,
-    layout=None,
-    device=None,
-    pin_memory=False,
-    requires_grad=False,
-    memory_format=torch.preserve_format,
-):
-    return torch.full(
-        [*self.size()],
-        fill_value,
-        dtype=dtype or self.dtype,
-        layout=layout or self.layout,
-        device=device or self.device,
-        requires_grad=requires_grad,
-    )
-
-
 @register_decomposition(aten.randint_like.default)
 def randint_like(self, high, *, dtype=None, device=None, **kwargs):
     return aten.randint.low(
