b4802d1f409176f22a77c841be7f74cb7f8cc83f
pytorchbot <soumith+bot@pytorch.org>
2023-09-07 17:36:29 -0700

    clean up

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -5556,9 +5556,6 @@ class Wait(ExternKernelAlloc):
         # Signal to codegen that our output buffer isn't safe to reuse
         return [self.inputs[0].codegen_reference()]
 
-    # def get_mutation_names(self):
-    #     return [self.inputs[0].codegen_reference()]
-
 
 class CollectiveKernel(ExternKernel):
     """
@@ -5621,7 +5618,6 @@ class InPlaceCollectiveKernel(CollectiveKernel):
 
     def __init__(self, layout, inputs, constant_args):
         super().__init__(layout, inputs, constant_args)
-        input_names = [t.codegen_reference() for t in self.inputs]
 
     def should_allocate(self):
         return False
@@ -5634,9 +5630,6 @@ class InPlaceCollectiveKernel(CollectiveKernel):
             wrapper.writeline(f"{output_name} = [{','.join(input_names)}] ")
         else:
             wrapper.writeline(f"{output_name} = {input_names[0]}")
-
-    def get_mutation_names(self):
-        return [self.inputs[0].codegen_reference()]
 
 
 class OutOfPlaceCollectiveKernel(CollectiveKernel):
@@ -5759,6 +5752,9 @@ class AllReduceCoalesced(InPlaceCollectiveKernel):
     def should_allocate(self):
         return False
 
+    def get_mutation_names(self):
+        return [self.inputs[0].codegen_reference()]
+
     @classmethod
     def create(
         cls,
@@ -5793,6 +5789,9 @@ class AllReduce(InPlaceCollectiveKernel):
     def __init__(self, layout, inputs, constant_args, reduce_op):
         super().__init__(layout, inputs, constant_args)
         self.reduce_op = reduce_op
+
+    def get_mutation_names(self):
+        return [self.inputs[0].codegen_reference()]
 
     @classmethod
     def create(
